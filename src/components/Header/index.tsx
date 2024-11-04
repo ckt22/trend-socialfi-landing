503133e3a2a001c1926f033d918dcd072b22dacf
ckt22 <tangck0202@gmail.com>
2024-11-04 10:13:06 +0800

    update: added packages and setup

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,55 +1,62 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/* eslint-disable no-console */
 "use client";
 
 import Image from "next/image";
 import Link from "next/link";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-// import {
-//   CHAIN_NAMESPACES,
-//   IAdapter,
-//   IProvider,
-//   WEB3AUTH_NETWORK,
-// } from "@web3auth/base";
-// import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
-// import { getDefaultExternalAdapters } from "@web3auth/default-evm-adapter";
-// import { Web3Auth, Web3AuthOptions } from "@web3auth/modal";
+import { IAdapter, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { getDefaultExternalAdapters } from "@web3auth/default-evm-adapter";
+import { Web3Auth, Web3AuthOptions } from "@web3auth/modal";
+import { web3AuthChainConfig, web3AuthClientId } from "@/contracts/config";
 
-// const privateKeyProvider = new EthereumPrivateKeyProvider({
-//   config: { chainConfig },
-// });
+const privateKeyProvider = new EthereumPrivateKeyProvider({
+  config: { chainConfig: web3AuthChainConfig },
+});
 
-// const web3AuthOptions: Web3AuthOptions = {
-//   clientId,
-//   web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
-//   privateKeyProvider,
-// };
-// const web3auth = new Web3Auth(web3AuthOptions);
+const web3AuthOptions: Web3AuthOptions = {
+  clientId: web3AuthClientId,
+  web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+  privateKeyProvider,
+};
+const web3auth = new Web3Auth(web3AuthOptions);
 
 export default function Header() {
-  // const [loggedIn, setLoggedIn] = useState(false);
+  const [provider, setProvider] = useState<IProvider | null>(null);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-  // useEffect(() => {
-  //   const init = async () => {
-  //     try {
-  //       const adapters = await getDefaultExternalAdapters({
-  //         options: web3AuthOptions,
-  //       });
-  //       adapters.forEach((adapter: IAdapter<unknown>) => {
-  //         web3auth.configureAdapter(adapter);
-  //       });
-  //       await web3auth.initModal();
-  //       setProvider(web3auth.provider);
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const adapters = await getDefaultExternalAdapters({
+          options: web3AuthOptions,
+        });
+        adapters.forEach((adapter: IAdapter<unknown>) => {
+          web3auth.configureAdapter(adapter);
+        });
+        await web3auth.initModal();
+        setProvider(web3auth.provider);
 
-  //       if (web3auth.connected) {
-  //         setLoggedIn(true);
-  //       }
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
+        if (web3auth.connected) {
+          setLoggedIn(true);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  //   init();
-  // }, []);
+    init();
+  }, []);
+
+  const login = async () => {
+    const web3authProvider = await web3auth.connect();
+    setProvider(web3authProvider);
+    if (web3auth.connected) {
+      setLoggedIn(true);
+    }
+  };
 
   return (
     <div className="bg-white">
@@ -59,13 +66,18 @@ export default function Header() {
           <Image src="/brt-logo.jpeg" height={35} width={120} alt="logo" />
         </div>
 
+        {/* NAVIGATIONS */}
         <div className="flex items-center space-between gap-x-5">
           <Link href="/">Questions</Link>
           <Link href="/">Questions</Link>
           <Link href="/">Leaderboard</Link>
         </div>
+        {/* USER DATA AND PROFILE */}
         <div className="pr-5 flex items-center gap-x-2">
-          <button className="text-white bg-blue-400 rounded px-3 py-1">
+          <button
+            onClick={login}
+            className="text-white bg-blue-400 rounded px-3 py-1"
+          >
             Web3
           </button>
           <button>Profile</button>
--- src/contracts/config.ts
+++ src/contracts/config.ts
@@ -1,10 +1,27 @@
 "use client";
 
+import { CHAIN_NAMESPACES } from "@web3auth/base";
 import { createPublicClient, createWalletClient, custom, http } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
-import { scrollSepolia } from "viem/chains";
+import { sepolia } from "viem/chains";
 
 export const publicClient = createPublicClient({
-  chain: scrollSepolia,
+  chain: sepolia,
   transport: http(),
 });
+
+export const web3AuthClientId =
+  "BAdZXDm_RIDI4htBGR4qMzMFlA89L5XLXwk5g0rXYTAdnFf1f6XV5Qim7StbPdkTzGFQOQkpe79AfYlboK8ACHM";
+
+export const web3AuthChainConfig = {
+  chainNamespace: CHAIN_NAMESPACES.EIP155,
+  chainId: "0xaa36a7",
+  rpcTarget: "https://rpc.ankr.com/eth_sepolia",
+  // Avoid using public rpcTarget in production.
+  // Use services like Infura, Quicknode etc
+  displayName: "Ethereum Sepolia Testnet",
+  blockExplorerUrl: "https://sepolia.etherscan.io",
+  ticker: "ETH",
+  tickerName: "Ethereum",
+  logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+};
